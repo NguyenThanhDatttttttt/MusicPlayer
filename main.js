dd1c9e09f51403c36cac67ce00fd1c9cbfde7353
NguyenThanhDatttttttt <thanhdat2308ct1@gmail.com>
2024-07-19 09:07:33 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+/*  1. Render song
+2. Scroll top handleEvents
+3. Play / pause / seek 
+4. CD rorate
+5. Next / prev
+6. Random   
+7. Next / Repeat when ended
+8. Active songs
+9. Scroll active song to view
+10.Play song when click*/
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+var songs: [
+    {   name: 'Lụy tình',
+        singer: 'Trung Dio',
+        path: 
+    }
+]
